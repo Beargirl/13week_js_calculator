96c6a7cc763b0189c2c94157452f487e2e58213a
Beargirl <cool.beargirl@yandex.ru>
2021-07-17 20:26:40 +0300

    calculator

---
+++ index.js
@@ -0,0 +1,23 @@
+function sum() {
+        let a = prompt("Первое число?", '');
+        let b = prompt("Второе число?", '');
+        alert(+a + +b);
+    }
+
+
+    function subtraction() {
+        let a = prompt("Первое число?", '');
+        let b = prompt("Второе число?", '');
+        alert(a - b);
+    }
+
+    function multiplication() {
+        let a = prompt("Первое число?", '');
+        let b = prompt("Второе число?", '');
+        alert(a * b);
+    }
+    function division() {
+        let a = prompt("Первое число?", '');
+        let b = prompt("Второе число?", '');
+        alert(a / b);
+    }
